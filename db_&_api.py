faaa4c1aefedcab688f84df8c0dde89c64cd1810
elcoronel <36834501+xelcoronelx@users.noreply.github.com>
2018-09-13 20:22:20 -0700

    Initial Files

---
+++ db_&_api.py
@@ -0,0 +1,7 @@
+from yelp.client import Client
+
+MY_API_KEY = "czZvBd4G8n5TN2iRTDxeJKusmURiYBnc4W1DpmpMP_ahnO7AR_b5aAjgxNmNZvFAvxH9AvNcppsQFXVUwOaTwZg6zSnS9O54-XohJmmRuBuqy3_4uw3dYTmM24KYW3Yx" #  Replace this with your real API key
+
+client = Client(MY_API_KEY)
+
+url
