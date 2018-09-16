28a20c50c87c8df9f94e258ea65edcc1c15ef321
elcoronel <36834501+xelcoronelx@users.noreply.github.com>
2018-09-15 17:51:20 -0700

    created json object with a path on flask, re-ran graysons file to change mex category and created db3 which is what we are currently using... I moved around the .js and .css files because we need to figure out how to link them to our json endpoint and to our html files.

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import pandas as pd
+import json
+from pprint import pprint
+
+engine = create_engine("sqlite:///3rd.db")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+businesses = Base.classes.maintable
+
+session = Session(engine)
+
 
 app = Flask(__name__)
 
@@ -9,6 +26,31 @@ def index():
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route('/data/jsondata')
+def establishments():
+
+    response = session.query(businesses).all()
+
+    results = []
+    for row in response:
+        row_dict = {
+            'RowId': row.RowID,
+            'City': row.City,
+            'Category': row.Category,
+            'Lat': row.Latitude,
+            'Long': row.Longitude,
+            'Name': row.Name,
+            'Phone': row.Phone,
+            'Rating': row.Rating,
+            'Price': row.Price,
+            'Reviews': row.Reviews,
+
+
+    }
+        results.append(row_dict)
+
+    return jsonify(results)
 
 
 if __name__ == "__main__":
