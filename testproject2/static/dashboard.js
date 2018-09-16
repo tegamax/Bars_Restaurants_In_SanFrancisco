b59fb6108880585b88da9ffec45b35c4cf86341a
elcoronel <36834501+xelcoronelx@users.noreply.github.com>
2018-09-16 16:58:02 -0700

    created a cleaner directory under test

---
+++ testproject2/static/dashboard.js
@@ -0,0 +1,26 @@
+// alert('testing javascript linking')
+
+
+
+// var url = "http://127.0.0.1:5000/data/jsondata";
+// var maindata = d3.json(url, function (json) {
+//     //code here
+//     console.log(json)
+// });
+
+
+// // const API_KEY = “pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ”;
+
+
+// var myMap = L.map("map", {
+//  center: [37.7749, -122.4194],
+//  zoom: 11
+// });
+
+
+// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "mapbox.streets",
+//   accessToken: API_KEY
+// }).addTo(map);
