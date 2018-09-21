f31d99f6e7bbe3367e17b1c4011c17a30b4fdba7
elcoronel <36834501+xelcoronelx@users.noreply.github.com>
2018-09-20 17:57:07 -0700

    main project code is in project 2 folder run that code

--- testproject2/static/js/charts.js
+++ testproject2/static/js/charts.js
@@ -7,39 +7,3 @@ var maindata2 = d3.json(url2, function (json) {
 });
 
 // isaac line chart
-
-
-
-const ctx = document.getElementById("lineChart");
-
-
-var jsonfile = {
-    "jsonarray": [{
-       "name": "Joe",
-       "age": 12
-    }, {
-       "name": "Tom",
-       "age": 14
-    }]
- };
- 
- var labels = jsonfile.jsonarray.map(function(e) {
-    return e.name;
- });
- var data = jsonfile.jsonarray.map(function(e) {
-    return e.age;
- });;
- 
- var config = {
-    type: 'line',
-    data: {
-       labels: labels,
-       datasets: [{
-          label: 'Graph Line',
-          data: data,
-          backgroundColor: 'rgba(0, 119, 204, 0.3)'
-       }]
-    }
- };
- console.log(config)
- var chart = new Chart(ctx, config);
