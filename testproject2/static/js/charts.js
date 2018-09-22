8e0e788a131233d4a754921393b8e7acd62b561a
Tega Ileleji <tegaileleji@Tegas-MBP.attlocal.net>
2018-09-21 22:02:31 -0700

    Modified Charts

--- testproject2/static/js/charts.js
+++ testproject2/static/js/charts.js
@@ -1,5 +1,5 @@
 
-// making sure data api is working..
+// making sure data api is working.
 var url2 = "http://127.0.0.1:5000/data/jsondata";
 var maindata2 = d3.json(url2, function (json) {
     //code here
